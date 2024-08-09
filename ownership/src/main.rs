32518576d8c7bd116d845e0642a4254ddb2eb9da
MarlonHamaekersPXL <mar.hamaekers@gmail.com>
2024-08-09 13:13:22 +0200

    borrowing and ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,48 +1,39 @@
 fn main() {
-    // ownership
+    // Borrowing -> action of creating a reference
+    // References and borrowing
+    let s1 = String::from("Hello");
+
+    let len = calculate_length(&s1);
+
+    // only 1 reference to a mutable value is allowed!
+    let mut s2 = String::from("Another Hello");
+    change(&mut s2);
+
+    println!("{s2}");
+    println!("The size of '{s1}' is {len}.");
+
+    // solution for multiple references
     {
-        // string made up of 3 parts. pointer, length and capacity. Placed on the stack.
-        // content is placed on the heap
-        // capacity is the amount of memory allocated for the string
-        // length is the amount of memory used by the string
-
-        // if 2 variables point to the same memory location, and when they go out of scope,
-        // the drop function gets called twice, which is a problem because it will try to free the same memory twice
-        // this is called a double free error
-        // copying pointer length and capacity without content is called a shallow copy.
-        // Rust doesn't perform shallow copies, instead it invalidates the first variable.
-        // This is called a move.
-
-        // Rust will never automatically create a deep copy of your data.
-        // Any copy will be considered inexpensive in terms of runtime performance.
-        // To make a copy call copy method
-        let s = String::from("I am valid");
-        let s1 = s.clone();
-
-        println!("{}", s);
-        let s2 = s;
-        // println!("{}", s); // This will give an error
-    } // drop function gets called to return the memory
-
-    {
-        let y = 5;
-        let x = y;
-        println!("{}", y);
-        println!("{}", x);
-        // possible because integers have a fixed value and get copied and placed on
-        // the stack
+        // new scope
+        let r1 = &mut s2;
+        println!("{r1}");
     }
+    {
+        let r2 = &s2;
+        let r3 = &s2;
+        println!("{r2} {r3}");
+        println!("{r2} {r3}");
+    }
+    let r2 = &mut s2;
+    println!("{r2}")
+}
 
-    // String literal
-    // let s = "hello";
+// not taking ownership
+// value it points to will not be dropped when reference stops being used
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
 
-    // String type
-    let mut my_string = String::from("hello");
-
-    // s.push_str(", world");
-    //
-    // println!("{s}");
-
-    my_string.push_str(", world");
-    println!("{}", my_string);
+fn change(some_string: &mut String){
+    some_string.push_str(", World");
 }
--- rust-grepper/src/main.rs
+++ rust-grepper/src/main.rs
@@ -1,19 +1,41 @@
-use std::env::args;
+// use std::env::args;
+use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it
+#[derive(Parser)]
 struct Cli {
+    /// pattern to look for
     pattern: String,
+    /// the path to the file to read
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = args().nth(1).expect("no pattern given");
-    let path = args().nth(2).expect("no path give");
 
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
-    // :? used for verbose debug trait
-    println!("Pattern: {:?}, path: {:?}", args.pattern, args.path)
+    let content = std::fs::read_to_string(&args.path).expect("could not read the file");
+
+    let result = std::fs::read_to_string("test.txt");
+    match result {
+        Ok(content) => {println!("File content: {}", content); }
+        Err(error) => {panic!("Can't deal with {}, just exit here!", error ); }
+        // Err(error) => {println!("Oh no: {}", error); }
+    }
+
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
+
 }
+// let pattern = args().nth(1).expect("no pattern given");
+// let path = args().nth(2).expect("no path give");
+//
+// let args = Cli {
+//     pattern: pattern,
+//     path: std::path::PathBuf::from(path),
+// };
+    // :? used for verbose debug trait
+    // println!("Pattern: {:?}, path: {:?}", args.pattern, args.path)
